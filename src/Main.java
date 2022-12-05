1baba82a29c103c1211d1225d52a33d9f540bf2f
v_qia <877473449@qq.com>
2022-12-05 17:44:11 +0800

    java

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        {
+            //输出hello world
+        /*
+        窗前明月光，
+        疑是地上霜。
+        举头望明月，
+        低头思故乡。
+         */
+            double money = 6.0;
+
+            System.out.println("Hello \n world!");
+            System.out.println(money);
+
+        }
+
+    }
+
+}
