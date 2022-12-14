5d2a04544fb5ec9655133d3047719a768aba6225
v_qia <877473449@qq.com>
2022-12-14 15:13:10 +0800

    方法调用的学习

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        {
-            //输出hello world
-        /*
-        窗前明月光，
-        疑是地上霜。
-        举头望明月，
-        低头思故乡。
-         */
-            double money = 6.0;
-            int a = 100;
+        // int a1 = add(100,200);
+        // int a2 = add(200,300);
+        // int a3 = add(300,400);
 
-            System.out.println("Hello \n world!");
-            System.out.println(money);
-            System.out.println(a);
-            System.out.println("hello");
-            System.out.println("你好java");
-            System.out.println("必须转型测试开发");
+        int num1 = 100;
+        int num2 = 200;
+        int sum = add(num1,num2);
+
+        System.out.println("两个数的和是："+sum);
+
+        printInfo();
 
         }
 
+        //两个数求和
+    public static int add(int n1,int n2) {
+
+        int sum = n1 + n2;
+        return sum;
+    }
+    // 打印公司信息
+    public static void printInfo(){
+        System.out.println("尚学堂");
+    }
     }
 
-}
