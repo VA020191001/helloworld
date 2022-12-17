0ef5c57c43e96847d77bdeb8c84cf1a7872ff36c
v_qia <877473449@qq.com>
2022-12-17 22:43:00 +0800

    面向对象学习welcome

---
+++ src/Welcome.java
@@ -0,0 +1,8 @@
+//  文件名: Welcome.java
+public class Welcome {
+        public static void main( String args[] )
+        {
+            System.out.println( "Welcome to Java Programming!" );
+        }  //结束main 方法的定义
+    }  // 结束类Welcome的定义
+
